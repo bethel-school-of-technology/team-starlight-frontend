dce22ff976f58f9bfb0038c0b72a9612af6b19c9
reben87 <arebenst@gmail.com>
2022-11-30 21:05:46 -0500

    working createUser

--- src/contexts/UserProvider.js
+++ src/contexts/UserProvider.js
@@ -3,10 +3,10 @@ import UserContext from "./UserContext";
 
 export const UserProvider = (props) => {
 
-    const baseUrl = "http://localhost:3001/api/users/";
+    const baseUrl = "http://localhost:3000/api/users/";
 
-    function createUser(username, password) {       
-        let user = { username, password };
+    function createUser(username, password, firstName, lastName) {       
+        let user = { username, password, firstName, lastName };
 
         return axios.post(baseUrl, user)
         .then(response => {
